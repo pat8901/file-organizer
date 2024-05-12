df832611b55b6cfd13471d552dbe080c73f488cf
Patrick <seitzpr@gmail.com>
2024-05-11 20:26:49 -0500

    Initial commit

---
+++ src/test/java/com/patco/fileorganizer/FileOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patco.fileorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
